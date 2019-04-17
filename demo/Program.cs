1ddcae0b06d8218f076724db0852eb6c81a25251
truis <156850822@qq.com>
2019-04-17 15:12:25 +0800

    '初始项目'

---
+++ demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("1212121212123333");
+            Console.WriteLine("****************************************************************************************************");
+            Console.ReadKey();
+        }
+    }
+}
